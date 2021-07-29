b260383a3a81c721070d4c021206f3eef084fe07
thang-tran-tt <quocthang0620@gmail.com>
2021-07-29 12:05:21 +0700

    edited all components

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,9 +1,20 @@
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+    if (message.attachments && message.attachments.length > 0){
+        return (
+            <img
+                src={message.attachments[0].file}
+                atl="message-attachment"
+                className="message-image"
+                style={{fload:'right'}}
+            />
+        );
+    }
+
     return (
-        <div>
-            MyMessage
+        <div className="message" style={{float:'right', marginRight:'18px', color:'white', backgroundColor:'#3B2A50'}}>
+            {message.text}
         </div>
     );
-}
+};
 
 export default MyMessage;
